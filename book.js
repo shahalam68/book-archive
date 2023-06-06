c06e627f8d6a2a72b2702ad16812d07930cbabac
Soykot Alam <a.k.mshahalam68@gmail.com>
2023-06-06 22:01:35 +0600

    show data

--- book.js
+++ book.js
@@ -7,5 +7,24 @@ const SearchBook = () => {
     const url = `https://openlibrary.org/search.json?q=${searchText}`
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => setBook(data))
 }
+ const setBook = data => {
+    // console.log(data.numFound)
+    const books = data.docs;
+    // console.log(books)
+    const divRow = document.getElementById('book-box');
+    books.forEach(book => {
+        console.log(book)
+        const bookBox = document.createElement('div');
+        bookBox.classList.add('col-md-3');
+        bookBox.innerHTML = `
+        <div class="shadow rounded p-3 m-2">
+            <h3>${book.title}</h3>
+             <p>descriptions</p>
+         </div>
+        `
+        
+        divRow.appendChild(bookBox);
+    });
+ }
