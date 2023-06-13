48722648fa7e4706c13c94d65a4578d78f93d07f
Soykot Alam <a.k.mshahalam68@gmail.com>
2023-06-13 11:41:35 +0600

    second last

--- book.js
+++ book.js
@@ -10,10 +10,22 @@ const SearchBook = () => {
     .then(data => setBook(data))
 }
  const setBook = data => {
-    // console.log(data.numFound)
+    console.log(data.numFound)
     const books = data.docs;
     // console.log(books)
     const divRow = document.getElementById('book-box');
+    const counter = document.getElementById('book-counter');
+    const error = document.getElementById('error-message');
+    counter.innerText = `${books.length} -- ${data.numFound}`;
+    if(data.numFound === 0){
+        error.innerText = "nothing to show"
+    }
+    else{
+        error.innerText = ''
+    }
+    while (divRow.lastElementChild) {
+        divRow.removeChild(divRow.lastElementChild);
+      }
     books.forEach(book => {
         // console.log(book)
         const bookBox = document.createElement('div');
@@ -21,11 +33,11 @@ const SearchBook = () => {
         const imgUrl = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`
         console.log(imgUrl);
         bookBox.innerHTML = `
-        <img class ="img-fluid" src="${imgUrl}" alt="">
-        <div class="shadow rounded p-3 m-2">
+            <img class ="img-fluid" src="${imgUrl}" alt="">
+            <div class="shadow rounded p-3 m-2">
             <h3>${book.title}</h3>
              <p>descriptions</p>
-         </div>
+            </div>
         ` 
         divRow.appendChild(bookBox);
     });
